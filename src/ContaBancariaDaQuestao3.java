11b99e819fc58de945cbfc7806f0a20c3524fdcb
Iuri Ramos <iuriramosdias@gmail.com>
2026-08-24 14:41:12 -0300

    questao 3 concluida

---
+++ src/ContaBancariaDaQuestao3.java
@@ -0,0 +1,55 @@
+public class ContaBancariaDaQuestao3 {
+    private String nomeDoTitular;
+    private double saldoDaConta;
+
+    public ContaBancariaDaQuestao3(String nome){
+        this.nomeDoTitular = nome;
+        this.saldoDaConta = 0;
+    }
+    public double deposito(double valor) {
+        if (valor > 0) {
+            this.saldoDaConta = this.saldoDaConta + valor;
+            return this.saldoDaConta;
+        } else {
+            IO.println("Depósito inválido");
+            return 0;
+        }
+    }
+    public double saque(double valor){
+            if(valor <= this.saldoDaConta && valor > 0){
+                this.saldoDaConta = this.saldoDaConta - valor;
+                return valor;
+
+            }
+            else{
+                IO.println("Saque inválido. Você não possui essa quantia.");
+                return 0;
+            }
+        }
+
+    public String getNomeDoTitular() {
+        return nomeDoTitular;
+    }
+
+    public double getSaldoDaConta() {
+        return saldoDaConta;
+    }
+
+    public void setNomeDoTitular(String nomeDoTitular) {
+        this.nomeDoTitular = nomeDoTitular;
+    }
+
+    public void setSaldoDaConta(double saldoDaConta) {
+        this.saldoDaConta = saldoDaConta;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaBancariaDaQuestao3{" +
+                "nomeDoTitular='" + nomeDoTitular + '\'' +
+                ", saldoDaConta=" + saldoDaConta +
+                '}';
+    }
+}
+
+
---
+++ src/questao3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class questao3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        IO.println("Digite o nome do titular da conta");
+        String nomeDoTitular = sc.nextLine();
+        ContaBancariaDaQuestao3 conta = new ContaBancariaDaQuestao3(nomeDoTitular);
+        IO.println("Digite o valor que deseja depositar");
+        double deposito = sc.nextDouble();
+        sc.nextLine();
+        conta.deposito(deposito);
+        IO.println(conta);
+        IO.println("Digite o valor que deseja sacar");
+        double saque = sc.nextDouble();
+        conta.saque(saque);
+        IO.println(conta);
+        sc.close();
+    }
+}
